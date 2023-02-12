e1df9b1fd49c35ad7044b1ce544760315e6b8134
Adrian Hatfield <99419677+adrianwhatfield@users.noreply.github.com>
2023-02-12 00:28:45 +0000

    Add basic character movement

---
+++ ancient_arena/Scenes/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+
+
+func _physics_process(delta):
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var directionX = Input.get_axis("left", "right")
+	if directionX:
+		velocity.x = directionX * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var directionY = Input.get_axis("up", "down")
+	if directionY:
+		velocity.y = directionY * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	if velocity.x < 0:
+		$Sprite2D.flip_h = true
+	elif velocity.x > 0:
+		$Sprite2D.flip_h = false
+
+	move_and_slide()
