a15ef411dccad21ec44e87fd609ee6de853c835c
Adrian Hatfield <adrianhatfield23@gmail.com>
2023-09-09 15:10:47 +0100

    Organise files

---
+++ ancient_arena/Scripts/arena.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$UI/UI/PlayerHealth.value = Stats.player["health_current"]
+	$UI/UI/PlayerHealth.max_value = Stats.player["health"]
+	$UI/UI/PlayerMagic.value = Stats.player["magic_current"]
+	$UI/UI/PlayerMagic.max_value = Stats.player["magic"]
