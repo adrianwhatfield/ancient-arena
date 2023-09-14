07ee1d78a2161c4ffec79aab26ad16848ff0e64a
Adrian Hatfield <adrianhatfield23@gmail.com>
2023-09-14 19:46:05 +0100

    Add settings

--- ancient_arena/Scripts/settings_menu.gd
+++ ancient_arena/Scripts/settings_menu.gd
@@ -1,19 +1,42 @@
 extends Control
 
+@onready var gamepad = $Settings/Gamepad/CheckBox
 
-# Called when the node enters the scene tree for the first time.
+var bus_name = "Master"
+var bus_index = 0
+
 func _ready():
-	$HBoxContainer/CheckBox.grab_focus()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	gamepad.grab_focus()
 
 func _on_check_box_pressed():
 	Stats.gamepad = true
 
-
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+
+func _on_fullscreen_check_toggled(button_pressed):
+	if button_pressed == true:
+		var resolution = DisplayServer.screen_get_size()
+		DisplayServer.window_set_size(resolution)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_resolution_item_selected(index):
+	match index:
+		0: DisplayServer.window_set_size(Vector2(1920, 1080))
+		1: DisplayServer.window_set_size(Vector2(1280, 800))
+		2: DisplayServer.window_set_size(Vector2(1280, 720))
+
+
+func _on_mute_toggled(button_pressed):
+	if button_pressed == true:
+		AudioServer.set_bus_mute(bus_index, true)
+	else:
+		AudioServer.set_bus_mute(bus_index, false)
+
+
+func _on_volume_value_changed(value):
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
