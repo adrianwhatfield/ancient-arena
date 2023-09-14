4b4588eb691e44e1052ae035d9ea76517af6c334
Adrian Hatfield <adrianhatfield23@gmail.com>
2023-09-14 19:49:48 +0100

    Reset on main_menu _ready()

--- ancient_arena/Scripts/main_menu.gd
+++ ancient_arena/Scripts/main_menu.gd
@@ -1,15 +1,9 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer/Button.grab_focus()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	Stats.reset()
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/arena.tscn")
