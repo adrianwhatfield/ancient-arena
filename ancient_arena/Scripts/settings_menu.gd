1f9fbfaf2d167a0e353d3e6940159d7decf40c9a
Adrian Hatfield <adrianhatfield23@gmail.com>
2023-09-15 14:42:34 +0100

    Improve settings menu

--- ancient_arena/Scripts/settings_menu.gd
+++ ancient_arena/Scripts/settings_menu.gd
@@ -1,15 +1,22 @@
 extends Control
 
 @onready var gamepad = $Settings/Gamepad/CheckBox
+@onready var fullscreen_check = $Settings/Fullscreen/FullscreenCheck
+@onready var volume = $Settings/Sound/Volume
 
 var bus_name = "Master"
 var bus_index = 0
 
 func _ready():
 	gamepad.grab_focus()
+	if Stats.gamepad == true:
+		gamepad.button_pressed = true
+	volume.value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		fullscreen_check.button_pressed = true
 
-func _on_check_box_pressed():
-	Stats.gamepad = true
+func _process(delta):
+	Stats.set_res(Stats.current_res)
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
@@ -26,9 +33,9 @@ func _on_fullscreen_check_toggled(button_pressed):
 
 func _on_resolution_item_selected(index):
 	match index:
-		0: DisplayServer.window_set_size(Vector2(1920, 1080))
-		1: DisplayServer.window_set_size(Vector2(1280, 800))
-		2: DisplayServer.window_set_size(Vector2(1280, 720))
+		0: Stats.current_res = Stats.res_1080
+		1: Stats.current_res = Stats.res_800
+		2: Stats.current_res = Stats.res_720
 
 
 func _on_mute_toggled(button_pressed):
@@ -40,3 +47,10 @@ func _on_mute_toggled(button_pressed):
 
 func _on_volume_value_changed(value):
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+
+
+func _on_check_box_toggled(button_pressed):
+	if button_pressed == true:
+		Stats.gamepad = true
+	else:
+		Stats.gamepad = false
--- ancient_arena/stats.gd
+++ ancient_arena/stats.gd
@@ -2,6 +2,11 @@ extends Node
 
 var gamepad = false
 
+var res_1080 = Vector2(1920, 1080)
+var res_800 = Vector2(1280, 800)
+var res_720 = Vector2(1280, 720)
+var current_res = res_720
+
 var health = 20
 var health_current = 20
 var health_regen = 0
@@ -16,3 +21,6 @@ func reset():
 	health_current = 20
 	magic_current = 50
 	score = 0
+
+func set_res(resolution):
+	DisplayServer.window_set_size(resolution)
