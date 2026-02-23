5a3a797b46df2f5523b8988cc72d8937d614c2ef
Adrian Hatfield <adrianhatfield23@gmail.com>
2026-02-23 09:44:57 +0000

    Add comments, edit functions

--- ancient_arena/Scripts/player.gd
+++ ancient_arena/Scripts/player.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 var stats = preload("res://Resources/player_stats.tres")
 var settings = preload("res://Resources/player_settings.tres")
 
+var spell_scene = preload("res://Scenes/Objects/spell.tscn")
+
 var direction = Vector2.ZERO
 var shoot_vector = Vector2.ZERO
 var speed = stats.speed.value
-
-var spell_scene = preload("res://Scenes/Objects/spell.tscn")
 
 signal spell_fired
 signal player_hit
@@ -27,7 +27,7 @@ func _physics_process(delta):
 		$Sprite2D.flip_h = false
 
 func _process(delta):
-	get_shoot()
+	$Wand.look_at(get_shoot_vector())
 	if Input.is_action_just_pressed("shoot") and stats.mana_current >= 10:
 		stats.mana_current -= 10
 		spell_fired.emit()
@@ -36,12 +36,14 @@ func _process(delta):
 	if stats.health_current <= 0:
 		get_tree().change_scene_to_file("res://Scenes/Scenes/Menu/death_screen.tscn")
 
+## Instantiate the spell and shoot it in the direction the player's looking
 func shoot():
 	var s = spell_scene.instantiate()
 	owner.add_child(s)
 	s.transform = $Wand/Shoot.global_transform
 
-func get_shoot():
+## Return the vector of where the wand should be looking at
+func get_shoot_vector() -> Vector2:
 	var screen_rect = DisplayServer.window_get_size()
 	var screen_height = screen_rect.x
 	var screen_width = screen_rect.y
@@ -53,9 +55,11 @@ func get_shoot():
 		shoot_vector.x *= (screen_width * 2.25)
 		shoot_vector.y *= (screen_height * 2.25)
 		
-		$Wand.look_at(shoot_vector)
+		return shoot_vector
 	elif settings.gamepad == false:
-		$Wand.look_at(get_global_mouse_position())
+		return get_global_mouse_position()
+	else:
+		return Vector2.ZERO
 
 
 func _on_timer_timeout():
